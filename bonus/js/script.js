3f933c40b07f113fe78384af81fa10e50258bb55
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-06-25 15:54:23 +0200

    ADD CLASS con JS

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -4,13 +4,13 @@ var listaNumeri = document.getElementById('lista-numeri')
 for (var i = 1; i < 101; i++) {
 
   if (i % 15 == 0) {
-    listaNumeri.innerHTML += "<li>" + fizz + buzz + "</li>";
+    listaNumeri.innerHTML += "<li class=\"fizzbuzz\">" + fizz + buzz + "</li>";
   }
   else if (i % 5 == 0) {
-      listaNumeri.innerHTML += "<li>" + buzz + "</li>";
+      listaNumeri.innerHTML += "<li class=\"buzz\">" + buzz + "</li>";
   }
   else if (i % 3 == 0) {
-      listaNumeri.innerHTML += "<li>" + fizz + "</li>";
+      listaNumeri.innerHTML += "<li class=\"fizz\">" + fizz + "</li>";
   }
   else {
       listaNumeri.innerHTML += "<li>" + i + "</li>";
